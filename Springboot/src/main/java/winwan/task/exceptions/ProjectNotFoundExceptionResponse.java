1d39e3680e731dbe0c8a73f393afd64e73c5e029
Seungwan Isaac Yuk <6winwan@gmail.com>
2019-07-11 16:06:55 -0600

    React Basic Setup

---
+++ Springboot/src/main/java/winwan/task/exceptions/ProjectNotFoundExceptionResponse.java
@@ -0,0 +1,23 @@
+package winwan.task.exceptions;
+
+public class ProjectNotFoundExceptionResponse {
+	
+	private String ProjectNotFound;
+
+	
+	
+	public ProjectNotFoundExceptionResponse(String projectNotFound) {
+	
+		ProjectNotFound = projectNotFound;
+	}
+
+	public String getProjectNotFound() {
+		return ProjectNotFound;
+	}
+
+	public void setProjectNotFound(String projectNotFound) {
+		ProjectNotFound = projectNotFound;
+	}
+	
+	
+}
