38bdd47ce708f850aa0d21c2caed60ba2331ebb9
Seungwan Isaac Yuk <6winwan@gmail.com>
2019-09-13 17:34:24 -0600

    React Secure Routes

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Layout/Header';
 import AddProject from './components/Project/AddProject';
 import UpdateProject from './components/Project/UpdateProject';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
@@ -17,7 +17,9 @@ import Login from './components/UserManagement/Login';
 import jwt_decode from "jwt-decode";
 import setJWTToken from "./securityUtils/setJWTToken";
 import { SET_CURRENT_USER } from './actions/types';
-import { logout } from './actions/securityActions'
+import { logout } from './actions/securityActions';
+import SecureRoute from "./securityUtils/secureRoute";
+
 const jwtToken = localStorage.jwtToken;
 
 if (jwtToken) {
@@ -55,12 +57,14 @@ class App extends Component {
             {
               //private Route
             }
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Route exact path="/addProject" component={AddProject} />
-            <Route exact path="/updateProject/:id" component={UpdateProject} />
-            <Route exact path="/projectBoard/:id" component={ProjectBoard} />
-            <Route exact path="/addProjectTask/:id" component={AddProjectTask} />
-            <Route exact path="/updateProjectTask/:backlog_id/:pt_id" component={UpdateProjectTask} />
+            <Switch>
+              <SecureRoute exact path="/dashboard" component={Dashboard} />
+              <SecureRoute exact path="/addProject" component={AddProject} />
+              <SecureRoute exact path="/updateProject/:id" component={UpdateProject} />
+              <SecureRoute exact path="/projectBoard/:id" component={ProjectBoard} />
+              <SecureRoute exact path="/addProjectTask/:id" component={AddProjectTask} />
+              <SecureRoute exact path="/updateProjectTask/:backlog_id/:pt_id" component={UpdateProjectTask} />
+            </Switch>
           </div>
         </Router>
       </Provider>
--- react-client/src/components/UserManagement/Register.js
+++ react-client/src/components/UserManagement/Register.js
@@ -16,7 +16,12 @@ class Register extends Component {
     }
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+  }
 
+  componentDidMount() {
+    if (this.props.security.validToken) {
+      this.props.history.push("/dashboard");
+    }
   }
 
   componentWillReceiveProps(nextProps) {
@@ -123,12 +128,14 @@ class Register extends Component {
 
 Register.propTypes = {
   createNewUser: PropTypes.func.isRequired,
-  errors: PropTypes.object.isRequired
+  errors: PropTypes.object.isRequired,
+  security: PropTypes.object.isRequired
 };
 
 
 const mapStateToProps = state => ({
-  errors: state.errors
+  errors: state.errors,
+  security: state.security
 });
 
 export default connect(mapStateToProps, {createNewUser})(Register);
